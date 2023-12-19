3d6176aa823417dba6f5ae40cdea135c88288d95
ZCY <ferrerooo@gmail.com>
2023-12-19 14:38:56 -0800

    Update async_hello2.py

--- samples/async/async_hello2.py
+++ samples/async/async_hello2.py
@@ -13,3 +13,22 @@ loop = asyncio.get_event_loop()
 tasks = [hello(), hello()]
 loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
+
+‘’‘
+import asyncio
+
+async def hello():
+    print('Hello world!')
+    await asyncio.sleep(1)
+    print('Hello again!')
+
+async def main():
+    tasks = [asyncio.create_task(hello()), asyncio.create_task(hello())]
+    await asyncio.wait(tasks)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
+
+
+’‘’

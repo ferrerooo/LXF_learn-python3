9e0dac19a0407164f47a5d16dba7858519bc609d
ZCY <ferrerooo@gmail.com>
2023-12-19 13:40:40 -0800

    Update S01_do_fork.py

--- samples/multitask/S01_do_fork.py
+++ samples/multitask/S01_do_fork.py
@@ -19,3 +19,9 @@ if pid == 0:
     print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
 else:
     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+
+'''
+Process (876) start...
+I (876) just created a child process (877).
+I am child process (877) and my parent is 876.
+'''
